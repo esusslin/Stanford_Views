43b66214f001756549fd3bdd5f725c51820c5d83
Emmet Susslin <esusslin@gmail.com>
2017-05-25 03:39:02 -0700

    face w/ eyes

---
+++ Faceit/Faceit/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Emmet Susslin on 5/25/17.
+//  Copyright © 2017 Emmet Susslin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+}
+
